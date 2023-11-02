70d92c71b7d4d2a1d8f6a94c6b8f92355ef6ab30
Shabadoo9 <brinstonzbpe@gmail.com>
2023-11-02 14:02:12 -0400

    heroku debug

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // Other options...
-});
+// Load environment variables from a .env file during development
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+// Connect to MongoDB using the MONGODB_URI environment variable
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+module.exports = mongoose.connection;
